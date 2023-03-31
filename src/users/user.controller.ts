6e56ee2a84bdc4331b9300e51267b2c1a9faf529
GeckoThePecko <bernie.gandin2@gmail.com>
2023-03-31 07:46:22 -0400

    Fix not returning answers

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -30,6 +30,14 @@ import {RejectionEmailService} from "../rejectionEmail/rejectionEmail.service";
                 eager: true,
                 alias: 'interviewResponses'
             },
+            'interviews.responses.textAnswers': {
+                eager: true,
+                alias: 'textAnswers3'
+            },
+            'interviews.responses.videoAnswers': {
+                eager: true,
+                alias: 'videoAnswers3'
+            },
             'interviews.questions': {
                 eager: true,
                 alias: 'interviewQuestions2'
